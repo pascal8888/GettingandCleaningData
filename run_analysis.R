c6e842a4ca9ee2bd6bf99daa1aa13cd63b8134a7
James Schacht <pascal888@yahoo.com>
2015-05-11 11:30:29 -0400

    Signed-off-by: James Schacht <pascal888@yahoo.com>

--- run_analysis.R
+++ run_analysis.R
@@ -1,2 +1,24 @@
-run_analysis.R
-path <- paste(getwd(),"/UCI%20HAR%20Dataset",sep="")
+path_train <- paste(getwd(),"/UCI HAR Dataset/train",sep="")
+flist_train <- list.files(path_train, full.names = TRUE, recursive=TRUE)
+for (file in flist_train){
+     if (!exists("dataset")){
+         dataset <- read.csv(file)
+     }
+    if (exists("dataset")){
+        temp_dataset <-read.csv(file)
+        dataset<-rbind(dataset, temp_dataset)
+        rm(temp_dataset)
+    }
+}
+# path2 <- paste(getwd(),"/UCI HAR Dataset/test",sep="")
+# flist1 <- list.files(path1, full.names = TRUE, recursive=TRUE)
+# for (file in flist1){
+#     if (!exists("dataset")){
+#         dataset <- read.table(file)
+#     }
+#     if (exists("dataset")){
+#         temp_dataset <-read.table(file)
+#         dataset<-rbind(dataset, temp_dataset)
+#         rm(temp_dataset)
+#     }
+# }
