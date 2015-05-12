e6ceee43f7c2571d1017d6f66ad2aefa118732e6
pascal8888 <pascal888@yahoo.com>
2015-05-12 11:03:40 -0400

    Signed-off-by: pascal8888 <pascal888@yahoo.com>

--- run_analysis.R
+++ run_analysis.R
@@ -1,24 +1,11 @@
+path_activity_labels <- paste(getwd(),"/UCI HAR Dataset/activity_labels.txt",sep="")
+df_activity_labels <- read.csv(path_activity_labels,header=FALSE)
 path_train <- paste(getwd(),"/UCI HAR Dataset/train",sep="")
 flist_train <- list.files(path_train, full.names = TRUE, recursive=TRUE)
-for (file in flist_train){
-     if (!exists("dataset")){
-         dataset <- read.csv(file)
-     }
-    if (exists("dataset")){
-        temp_dataset <-read.csv(file)
-        dataset<-rbind(dataset, temp_dataset)
-        rm(temp_dataset)
-    }
-}
-# path2 <- paste(getwd(),"/UCI HAR Dataset/test",sep="")
-# flist1 <- list.files(path1, full.names = TRUE, recursive=TRUE)
-# for (file in flist1){
-#     if (!exists("dataset")){
-#         dataset <- read.table(file)
-#     }
-#     if (exists("dataset")){
-#         temp_dataset <-read.table(file)
-#         dataset<-rbind(dataset, temp_dataset)
-#         rm(temp_dataset)
-#     }
-# }
+df_subject <- read.csv(flist_train[1])
+df_x <- read.csv(flist_train[2],header=TRUE)
+names(df_x) <- "X2"
+df_y <- read.csv(flist_train[3])
+df_combined<- cbind(df_subject,df_x,df_y)
+
+## rm(list = ls())
