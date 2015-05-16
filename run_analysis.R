924738717a0391c793487b5821160c0a86e84b28
James Schacht <pascal888@yahoo.com>
2015-05-16 06:29:12 -0400

    Signed-off-by: James Schacht <pascal888@yahoo.com>

--- run_analysis.R
+++ run_analysis.R
@@ -1,16 +1,13 @@
 ##
 ## STEP 1 <- Merges the training and the test sets to create one data set.
 ##
-##  Get the source data set
-if (!file.exists("uci_dataset.zip")) {
+##  Get the source data set and extract in working directory (only downloads and unzips if it does not already exist)
+if (!file.exists("source_dataset.zip")) {
     download.file("https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip ",
-                  "source_data.zip", "curl", TRUE)
+                  "source_dataset.zip", "auto", TRUE)
 }
-
-# Extract the archive contents to the current working subdirectory.
-
 if (!file.exists("UCI HAR Dataset")) {
-    unzip("source_data.zip")
+    unzip("source_dataset.zip")
 }
 ##Read the files into vectors for the labels and data frames to hold the values of subject, activity, and measures
 vctr_activity_names <- read.table("UCI HAR Dataset/activity_labels.txt", as.is = TRUE)[, 2] ##col 2 only
@@ -21,7 +18,7 @@ df_test_subject <- read.table("UCI HAR Dataset/test/subject_test.txt")
 df_train_subject <- read.table("UCI HAR Dataset/train/subject_train.txt")
 df_test_measures <- read.table("UCI HAR Dataset/test/X_test.txt")
 df_train_measures <- read.table("UCI HAR Dataset/train/X_train.txt")
-##Combine the data frames
+##Combine the data frames (append columns to the right and rows below)
 df_combined <-
     cbind(
         rbind(df_train_subject, df_test_subject),
@@ -47,15 +44,15 @@ vctr_activity_names <-
     )
 df_combined$Activity <- vctr_activity_names [df_combined$Activity]
 ##
-## STEP 4 <- Uses descriptive activity names to name the activities in the data set
+## STEP 4 <- Appropriately labels the data set with descriptive variable names.
 ##
-##Remove the "Freq" meausures.  Otherwise keep the variable names as column headers - changing them would require explanation, this is tidyer. The features_info.txt file, found in the root of the data file unzipped, gives a very good explanation of the meanings.
+## Partially done in the process of step 2. Additional step - remove the "Freq" meausures.  Otherwise keep the variable names as column headers - changing them would require explanation, this is tidyer. The features_info.txt file, found in the root of the unzipped source data file, gives a very good explanation of the meanings.
 df_combined <- df_combined[, grep("Freq",invert=TRUE,names(df_combined))]
 ##
 ## STEP 5 <- From the data set in step 4, creates a second, independent tidy data set with the average of each variable for each activity and each subject. Good luck!
 ##
 library(reshape2)
-finaldata <- melt(df_combined, id=c(Subject","Activity"), measure.vars = colnames(df_combined[, grep("mean|std", colnames(df_combined))]))
+finaldata <- melt(df_combined, id=c("Subject","Activity"), measure.vars = colnames(df_combined[, grep("mean|std", colnames(df_combined))]))
 finaldata <- dcast(finaldata, Subject + Activity ~ variable, mean)
 
 # Save tidy dataset to file.
